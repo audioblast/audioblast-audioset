470a299fb2a689f7f4e13a2e9742789e4f58be18
Ed Baker <edwbaker@gmail.com>
2020-04-08 14:38:39 +0100

    Simplify output.

--- audioblast.py
+++ audioblast.py
@@ -1,48 +1,3 @@
-# Copyright 2017 The TensorFlow Authors All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
-r"""A simple demonstration of running VGGish in inference mode.
-
-This is intended as a toy example that demonstrates how the various building
-blocks (feature extraction, model definition and loading, postprocessing) work
-together in an inference context.
-
-A WAV file (assumed to contain signed 16-bit PCM samples) is read in, converted
-into log mel spectrogram examples, fed into VGGish, the raw embedding output is
-whitened and quantized, and the postprocessed embeddings are optionally written
-in a SequenceExample to a TFRecord file (using the same format as the embedding
-features released in AudioSet).
-
-Usage:
-  # Run a WAV file through the model and print the embeddings. The model
-  # checkpoint is loaded from vggish_model.ckpt and the PCA parameters are
-  # loaded from vggish_pca_params.npz in the current directory.
-  $ python vggish_inference_demo.py --wav_file /path/to/a/wav/file
-
-  # Run a WAV file through the model and also write the embeddings to
-  # a TFRecord file. The model checkpoint and PCA parameters are explicitly
-  # passed in as well.
-  $ python vggish_inference_demo.py --wav_file /path/to/a/wav/file \
-                                    --tfrecord_file /path/to/tfrecord/file \
-                                    --checkpoint /path/to/model/checkpoint \
-                                    --pca_params /path/to/pca/params
-
-  # Run a built-in input (a sine wav) through the model and print the
-  # embeddings. Associated model files are read from the current directory.
-  $ python vggish_inference_demo.py
-"""
 
 from __future__ import print_function
 
@@ -99,7 +54,7 @@ def main(_):
     soundfile.write(wav_file, samples, sr, format='WAV', subtype='PCM_16')
     wav_file.seek(0)
   examples_batch = vggish_input.wavfile_to_examples(wav_file)
-  print(examples_batch)
+  #print(examples_batch)
 
   # Prepare a postprocessor to munge the model embeddings.
   pproc = vggish_postprocess.Postprocessor(FLAGS.pca_params)
@@ -118,7 +73,8 @@ def main(_):
     [embedding_batch] = sess.run([embedding_tensor],
                                  feed_dict={features_tensor: examples_batch})
     postprocessed_batch = pproc.postprocess(embedding_batch)
-    print(postprocessed_batch)
+    #print(postprocessed_batch)
+    print(np.array2string(postprocessed_batch, separator=', '))
 
 if __name__ == '__main__':
   tf.app.run()
